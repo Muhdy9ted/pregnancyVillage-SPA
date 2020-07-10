a6d6031c8e9f23e09477b3ee84f57eee7bacc3fe
muhammedu9ted@outlook.com <muhammedu9ted@outlook.com>
2020-07-10 21:28:58 +0100

    environment vairable turn production mode on

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://pregnancy-village.herokuapp.com/v1/forum/'
 };
 
