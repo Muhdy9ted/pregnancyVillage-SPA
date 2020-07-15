9398f75fcfe33a97a547671cf83992326eaed6c1
muhammedu9ted@outlook.com <muhammedu9ted@outlook.com>
2020-07-15 09:45:20 +0100

    strted admin module, updated reactions func

--- src/app/components/Admin-Module/admin.module.ts
+++ src/app/components/Admin-Module/admin.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin.routing.module';
+import { ErrorInterceptorProvider } from 'src/app/_shared/services/error.interceptor';
+import { ForumService } from 'src/app/_shared/services/forum.service';
+import { AuthService } from 'src/app/_shared/services/auth.service';
+import { AlertifyService } from 'src/app/_shared/services/alertify.service';
+import { UserService } from 'src/app/_shared/services/user.service';
 
 @NgModule({
     declarations: [AdminComponent],
@@ -12,7 +17,8 @@ import { AdminRoutingModule } from './admin.routing.module';
         CommonModule,
         FormsModule,
         AdminRoutingModule
-    ]
+    ],
+    providers: [ErrorInterceptorProvider, ForumService, AuthService, AlertifyService, UserService]
 })
 
 export class AdminModule {}
